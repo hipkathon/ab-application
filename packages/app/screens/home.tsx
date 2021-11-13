e293aa609f5660b0dfdba2cd54cd42f845b9b3b0
Jun Kyu <kuk941025@gmail.com>
2021-11-13 15:45:05 +0900

    Add tailwind

--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Text } from 'react-native'
+import tw from 'twrnc'
 
 export default function HomeScreen() {
-  return <></>
+  return <Text style={tw`bg-blue-200`}>Asd</Text>
 }
